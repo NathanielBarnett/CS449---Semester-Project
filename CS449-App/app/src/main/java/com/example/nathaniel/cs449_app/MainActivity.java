4d428d24c429096802a6f5a33ccdc94847b97b2b
Nathaniel A. Barnett <thegrtn@gmail.com>
2017-09-23 21:48:13 -0500

    Fixing Appbar
    Enabled appbar functionality and about button.

--- CS449-App/app/src/main/java/com/example/nathaniel/cs449_app/MainActivity.java
+++ CS449-App/app/src/main/java/com/example/nathaniel/cs449_app/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.nathaniel.cs449_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
